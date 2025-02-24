a8646cb77d668cf6a8fa6db74abc2f954d5fc778
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-02-24 21:20:36 +0100

    it works

--- src/js/appwriteAuth.js
+++ src/js/appwriteAuth.js
@@ -5,7 +5,56 @@ export const checkUserSession = async () => {
     const user = await account.get();
     return user;
   } catch (error) {
+    if (error.code === 401) {
+      console.log("the error code was 401");
+      return null;
+    }
     console.log("Error getting account status: ", error);
     return null;
+  }
+};
+
+export const login = async () => {
+  const baseUrl = window.location.origin;
+  console.log("account", account);
+
+  try {
+    const response = await account.createOAuth2Token(
+      "github",
+      `${baseUrl}/guestbook`,
+      `${baseUrl}/guestbook`
+    );
+
+    console.log("OAuth response: ", response);
+  } catch (error) {
+    console.error("Login error: ", error);
+  }
+};
+
+export const logout = async () => {
+  try {
+    account.deleteSession("current");
+  } catch (error) {
+    console.error("Logout failed:", error);
+  }
+};
+
+export const handleOAuthCallBack = async () => {
+  const params = new URLSearchParams(window.location.search);
+  const secret = params.get("secret");
+  const userId = params.get("userId");
+
+  console.log("secret: ", secret);
+  console.log("UserId:", userId);
+
+  if (secret) {
+    try {
+      await account.createSession(userId, secret);
+      console.log("Session created successfully!");
+
+      window.history.replaceState({}, document.title, window.location.pathname);
+    } catch (error) {
+      console.error("Error creating session with OAuth callback: ", error);
+    }
   }
 };
--- src/pages/GuestBook.jsx
+++ src/pages/GuestBook.jsx
@@ -1,8 +1,46 @@
+import { useEffect, useState } from "react";
+import {
+  checkUserSession,
+  handleOAuthCallBack,
+  login,
+  logout,
+} from "../js/appwriteAuth";
+
 export default function GuestBook() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      await handleOAuthCallBack();
+      const userData = await checkUserSession();
+      console.log("Fetched User: ", userData);
+      setUser(userData);
+    };
+
+    fetchUser();
+  }, []);
+
   return (
-    <div>
+    <div className="flex flex-col items-center">
       <h1>Guestbook</h1>
-      <div>Welcome to the guestbook page!</div>
+      {user ? (
+        <div>
+          <p>Welcome, {user.name} </p>
+          <button
+            onClick={logout}
+            className="bg-black text-white rounded-lg px-3 py-2 cursor-pointer"
+          >
+            Logout
+          </button>
+        </div>
+      ) : (
+        <button
+          onClick={login}
+          className="bg-black text-white rounded-lg px-3 py-2 cursor-pointer"
+        >
+          Login with GitHub
+        </button>
+      )}
     </div>
   );
 }
