3ea13e847dba4175e38eed5a351358fa12aa0916
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-02-25 23:17:09 +0100

    prose tailwind typography

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -3,7 +3,7 @@ import testMarkdown from "../markdown/test123.md?raw";
 
 export default function BlogPost() {
   return (
-    <article className="p-4 max-w-screen-xl mx-auto">
+    <article className="prose prose-invert   p-4 max-w-screen-xl mx-auto ">
       <h2>Posts</h2>
       <Markdown>{testMarkdown}</Markdown>
     </article>
