96a78e2182f8bfe671082985b875a91af3f63d1a
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-02-25 20:42:01 +0100

    loader

--- src/components/LowPage.jsx
+++ src/components/LowPage.jsx
@@ -1,11 +1,11 @@
 import Contact from "./Contact";
-import FlipCounter from "./FlipCounter";
+import CounterContainer from "./CounterContainer";
 
 export default function LowPage() {
   return (
     <div className="flex justify-between gap-8 sm:gap-0 sm:flex-row flex-col items-end p-4">
       <Contact />
-      <FlipCounter count={889} />
+      <CounterContainer />
     </div>
   );
 }
