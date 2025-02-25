82bc09db60915ccb3f6fdac17309869d57f96b2c
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-02-25 14:15:11 +0100

    styling

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,8 @@
+import { FaGithub } from "react-icons/fa";
+
 export default function Contact() {
   return (
-    <div className="mt-6 ">
+    <div className="mt-6 text-white">
       {" "}
       <h3 className="text-3xl text-center mb-2">Contact</h3>
       <div className="flex justify-center flex-col ">
@@ -21,12 +23,8 @@ export default function Contact() {
           </p>
         </div>
         <div className="flex justify-center items-center gap-2 mt-2 mr-5">
-          <img
-            src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original-wordmark.svg"
-            alt="Linkedin"
-            className="w-12 h-12"
-          />
-          <span>Github:</span>
+          <FaGithub size={40} />
+          <span className="ml-1">Github:</span>
           <p className="underline">
             <a href="https://github.com/Bakek-lang" target="_blank">
               Bakek-Lang
