0709926bc836f35a9bb1be07bb86e6881cb89c28
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-02-26 11:28:44 +0100

    blog posts array

---
+++ src/js/blogPosts.js
@@ -0,0 +1,12 @@
+import testMarkdown from "../markdown/test123.md?raw";
+
+const blogPosts = [
+  {
+    id: "test-post",
+    title: "Test Blog Post",
+    excerpt: "This is a test blog post.",
+    content: testMarkdown,
+  },
+];
+
+export default blogPosts;
