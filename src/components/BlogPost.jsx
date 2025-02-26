f503880b4ce48f9718fe0786870b4332c6f351f6
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-02-26 11:21:56 +0100

    refactor

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,8 +1,7 @@
 import Markdown from "markdown-to-jsx";
-import testMarkdown from "../markdown/test123.md?raw";
 import CodeBlock from "./CodeBlock";
 
-export default function BlogPost() {
+export default function BlogPost({ content }) {
   return (
     <article className="prose prose-invert   p-4 max-w-screen-xl mx-auto ">
       <h2>Posts</h2>
@@ -15,7 +14,7 @@ export default function BlogPost() {
           },
         }}
       >
-        {testMarkdown}
+        {content}
       </Markdown>
     </article>
   );
--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,10 +1,11 @@
 import BlogPost from "../components/BlogPost";
+import testMarkdown from "../markdown/test123.md?raw";
 
 export default function BlogPage() {
   return (
     <div className="text-white ">
       <h1 className="text-center">BLOG SITE</h1>
-      <BlogPost />
+      <BlogPost content={testMarkdown} />
     </div>
   );
 }
