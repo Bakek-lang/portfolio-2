9ea5503c1272f69cdd334de4b2f47fe5d75c98e6
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-02-26 11:14:40 +0100

    code syntax highlighter

--- src/components/BlogPost.jsx
+++ src/components/BlogPost.jsx
@@ -1,11 +1,22 @@
 import Markdown from "markdown-to-jsx";
 import testMarkdown from "../markdown/test123.md?raw";
+import CodeBlock from "./CodeBlock";
 
 export default function BlogPost() {
   return (
     <article className="prose prose-invert   p-4 max-w-screen-xl mx-auto ">
       <h2>Posts</h2>
-      <Markdown>{testMarkdown}</Markdown>
+      <Markdown
+        options={{
+          overrides: {
+            code: {
+              component: CodeBlock,
+            },
+          },
+        }}
+      >
+        {testMarkdown}
+      </Markdown>
     </article>
   );
 }
---
+++ src/components/CodeBlock.jsx
@@ -0,0 +1,11 @@
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { tomorrow } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+export default function CodeBlock({ className, children, ...props }) {
+  const language = className ? className.replace(/^(lang|language)-/, "") : "";
+  return (
+    <SyntaxHighlighter language={language} style={tomorrow} {...props}>
+      {children}
+    </SyntaxHighlighter>
+  );
+}
