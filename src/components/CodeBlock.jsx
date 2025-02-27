a33a7ed305654d49b4bf31c09d2389d36cabc46a
Bakek-lang <119268924+Bakek-lang@users.noreply.github.com>
2025-02-27 13:50:28 +0100

    remove styling for terminal

--- src/components/CodeBlock.jsx
+++ src/components/CodeBlock.jsx
@@ -1,15 +1,11 @@
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import {
-  tomorrow,
-  vscDarkPlus,
-} from "react-syntax-highlighter/dist/esm/styles/prism";
+import { tomorrow } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 export default function CodeBlock({ className, children, ...props }) {
   const language = className ? className.replace(/^(lang|language)-/, "") : "";
 
-  const style = language === "terminal" ? vscDarkPlus : tomorrow;
   return (
-    <SyntaxHighlighter language={language} style={style} {...props}>
+    <SyntaxHighlighter language={language} style={tomorrow} {...props}>
       {children}
     </SyntaxHighlighter>
   );
